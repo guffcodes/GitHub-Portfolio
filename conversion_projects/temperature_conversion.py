e2efbfa7877bdb69f9443858b8d195ab4ae709ec
john <163083785+guffcodes@users.noreply.github.com>
2024-11-10 21:45:17 -0500

    Delete conversion_projects directory

--- conversion_projects/temperature_conversion.py
+++
@@ -1,8 +0,0 @@
-# ask user to input a number in celsius to be converted into fahrenheit
-
-celsius = input("Enter a temperature in Celsius: ") # user input
-
-fahr = int(celsius) * 9 / 5 + 32 # conversion formula
-
-# print
-print (("The temperature in degrees Fahrenheit is: "), fahr)
